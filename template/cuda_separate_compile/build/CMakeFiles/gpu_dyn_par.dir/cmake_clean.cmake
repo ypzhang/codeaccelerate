a75106ba4b9a77e59c79632afb39191b2f9e9383
ypzhang <ypzhang@stoneridgetechnology.com>
2014-10-22 16:14:26 -0400

    add a template

---
+++ template/cuda_separate_compile/build/CMakeFiles/gpu_dyn_par.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gpu_dyn_par.dir/src/./gpu_dyn_par_generated_main.cu.o"
+  "CMakeFiles/gpu_dyn_par.dir/./gpu_dyn_par_intermediate_link.o"
+  "gpu_dyn_par.pdb"
+  "gpu_dyn_par"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang)
+  include(CMakeFiles/gpu_dyn_par.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
